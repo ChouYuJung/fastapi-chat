551625c47611773bf949c64946b01811af00f292
Allen Chou <f1470891079@gmail.com>
2023-11-28 19:34:48 +0800

    Use ConfigDict

--- app/app/schemas/oauth.py
+++ app/app/schemas/oauth.py
@@ -1,9 +1,12 @@
 from typing import Optional, Text
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
+# Strip any space
 class User(BaseModel):
+    model_config = ConfigDict(str_strip_whitespace=True)
+
     username: Text = Field(..., min_length=4, max_length=32, pattern="^[a-zA-Z0-9_-]+$")
     email: EmailStr
     organization: Text
