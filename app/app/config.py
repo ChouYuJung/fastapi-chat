8d3bb885a83a232dfbe2b2ad8d6f46f3d766d479
Allen Chou <f1470891079@gmail.com>
2023-11-28 19:07:54 +0800

    Add app name and version tag

--- app/app/config.py
+++ app/app/config.py
@@ -2,6 +2,12 @@ from typing import Text
 
 from pydantic_settings import BaseSettings
 
+from .version import VERSION
+
 
 class Settings(BaseSettings):
     app_name: Text = "fastapi-app-service"
+    app_version: Text = VERSION
+
+
+settings = Settings()
--- app/app/main.py
+++ app/app/main.py
@@ -1,12 +1,13 @@
 from typing import Annotated
 
+from app.config import settings
 from app.utils.common import get_system_info
 from app.utils.oauth import oauth2_scheme
 from fastapi import Depends, FastAPI
 
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(title=settings.app_name.title(), version=settings.app_version)
 
     @app.get("/")
     async def root():
