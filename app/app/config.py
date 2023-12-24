cc21d9eaffee3c3dbfc78904623f2977e08f2886
Allen Chou <f1470891079@gmail.com>
2023-12-24 14:45:54 +0800

    Add color logger

--- app/app/config.py
+++ app/app/config.py
@@ -1,8 +1,15 @@
+import logging
+import logging.config
+from datetime import datetime
 from typing import Text
 
+import pytz
+from colorama import Fore, Style, init
 from pydantic_settings import BaseSettings
 
 from .version import VERSION
+
+init(autoreset=True)
 
 
 class Settings(BaseSettings):
@@ -19,3 +26,75 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class IsoDatetimeFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        record_datetime = datetime.fromtimestamp(record.created).astimezone(
+            pytz.timezone("Asia/Taipei")
+        )
+        t = record_datetime.strftime("%Y-%m-%dT%H:%M:%S")
+        z = record_datetime.strftime("%z")
+        ms_exp = record_datetime.microsecond // 1000
+        s = f"{t}.{ms_exp:03d}{z}"
+        return s
+
+
+class ColoredIsoDatetimeFormatter(IsoDatetimeFormatter):
+    COLORS = {
+        "WARNING": Fore.YELLOW,
+        "INFO": Fore.GREEN,
+        "DEBUG": Fore.BLUE,
+        "CRITICAL": Fore.RED,
+        "ERROR": Fore.RED,
+    }
+    MSG_COLORS = {
+        "WARNING": Fore.YELLOW,
+        "INFO": Fore.GREEN,
+        "CRITICAL": Fore.RED,
+        "ERROR": Fore.RED,
+    }
+
+    def format(self, record):
+        levelname = record.levelname
+        if levelname in self.COLORS:
+            record.levelname = (
+                self.COLORS[levelname] + f"{levelname:8s}" + Style.RESET_ALL
+            )
+            record.name = Fore.BLUE + record.name + Style.RESET_ALL
+            if levelname in self.MSG_COLORS:
+                record.msg = self.COLORS[levelname] + record.msg + Style.RESET_ALL
+        return super(ColoredIsoDatetimeFormatter, self).format(record)
+
+
+def default_logging_config():
+    d = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "basic_formatter": {
+                "()": ColoredIsoDatetimeFormatter,
+                "format": "%(asctime)s %(levelname)-8s %(name)s  - %(message)s",
+            },
+            "message_formatter": {"format": "%(message)s"},
+        },
+        "handlers": {
+            "console_handler": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "basic_formatter",
+            }
+        },
+        "loggers": {
+            settings.app_name: {
+                "level": "DEBUG",
+                "handlers": ["console_handler"],
+                "propagate": True,
+            }
+        },
+    }
+    return d
+
+
+logging.config.dictConfig(default_logging_config())
+logger = logging.getLogger(settings.app_name)
