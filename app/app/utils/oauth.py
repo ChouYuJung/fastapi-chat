b9e12e77643b4822370575f9c87d5dcde97d5e68
Allen Chou <f1470891079@gmail.com>
2024-08-20 22:50:47 +0800

    ## Enhanced `oauth.py` with docstrings and code refinement
    - Added docstrings to functions for improved clarity
    - Streamlined the `create_access_token` function's logic for better readability

--- app/app/utils/oauth.py
+++ app/app/utils/oauth.py
@@ -14,14 +14,18 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.token_url)
 
 
 def verify_password(plain_password: Text, hashed_password: Text) -> bool:
+    """Verify the given password with the hashed password."""
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
+    """Get the password hash for the given password."""
     return pwd_context.hash(password)
 
 
 def authenticate_user(fake_db, username: Text, password: Text) -> Optional["UserInDB"]:
+    """Authenticate a user with the given username and password."""
+
     user = get_user(fake_db, username)
     if not user:
         return None
@@ -35,12 +39,13 @@ def create_access_token(
     expires_delta: Optional[timedelta] = None,
     key: Text = settings.SECRET_KEY,
     algorithm: Text = settings.ALGORITHM,
-):
+) -> Text:
+    """Create an access token with the given data."""
+
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(UTC) + expires_delta
-    else:
-        expire = datetime.now(UTC) + timedelta(minutes=15)
+    expire = datetime.now(UTC) + (
+        expires_delta if expires_delta else timedelta(minutes=15)
+    )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, key, algorithm=algorithm)
     return encoded_jwt
